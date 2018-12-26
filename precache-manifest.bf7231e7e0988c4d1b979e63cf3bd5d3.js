57b36523c1818887cd52b334f01860cf80fc0c69
Max Stern <stern.maxwell@gmail.com>
2018-12-26 01:52:23 -0600

    Updates

---
+++ precache-manifest.bf7231e7e0988c4d1b979e63cf3bd5d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "35115efbc6d12b470e85",
+    "url": "./static/css/main.074d70fc.chunk.css"
+  },
+  {
+    "revision": "35115efbc6d12b470e85",
+    "url": "./static/js/main.35115efb.chunk.js"
+  },
+  {
+    "revision": "cdcaf3a71802508645b7",
+    "url": "./static/js/1.cdcaf3a7.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "7e5114c80f69412ad523ffa837d23197",
+    "url": "./static/media/theinhardtreg-webfont.7e5114c8.ttf"
+  },
+  {
+    "revision": "47801c98b86da397fd03ed97be56dd8e",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1175f7212aaad5d323b3b39384b886bc.js"
+  "./precache-manifest.bf7231e7e0988c4d1b979e63cf3bd5d3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
