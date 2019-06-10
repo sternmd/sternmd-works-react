ff333306df147926e15bebc957d039fcbb9bb58e
Max Stern <stern.maxwell@gmail.com>
2019-06-09 22:27:47 -0700

    services page

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -4,33 +4,39 @@ import { Link } from 'react-router-dom';
 
 import BackArrow from '../layout/BackArrow';
 
-const Services = () =>
+const Services = () => (
   <div>
-      <Container>
-        <Columns>
-          <Column>
-            <p>My freelance services include:
-            <br/>
-            <br/>
-            <span class="red">
-            Web Design and Development.
-            <br/>
-            Search Engine Optimization (SEO).
-            <br/>
-            Digital Marketing Strategy.
-            <br/>
-            Brand Identity.
+    <Container>
+      <Columns>
+        <Column>
+          <p>
+            My freelance services include:
+            <br />
+            <br />
+            <span class='red'>
+              Web Design and Development.
+              <br />
+              Search Engine Optimization (SEO).
+              <br />
+              {/* Digital Marketing Strategy.
+              <br /> */}
+              Email & Social Media Marketing.
+              <br />
+              Brand/Corporate Identity.
+              <br />
+              Computer Lessons/Tutor.
             </span>
-            <br/>
-            <br/>
-            I particularly enjoy working with startups, nonprofits, and small business owners. I believe that honest dialogue with clients and hard work are the keys to creating a remarkable product. <Link to='/contact'>Contact me</Link> anytime!
-            </p>
-
-
-          </Column>
-        </Columns>
-      </Container>
-      <BackArrow />
+            <br />
+            <br />I particularly enjoy working with startups, nonprofits, and
+            small business owners. I believe that honest dialogue with clients
+            and hard work are the keys to creating a remarkable product.{' '}
+            <Link to='/contact'>Contact me</Link> anytime!
+          </p>
+        </Column>
+      </Columns>
+    </Container>
+    <BackArrow />
   </div>
+);
 
 export default Services;
