371a969a00fc61519d7df4fb1bbc790d56ad24be
Max Stern <stern.maxwell@gmail.com>
2019-06-10 13:24:44 -0700

    Updates

---
+++ precache-manifest.26dbd8c6e40277290d468c69699c294e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dde40f7221bbab8807ef",
+    "url": "./static/css/main.00011176.chunk.css"
+  },
+  {
+    "revision": "dde40f7221bbab8807ef",
+    "url": "./static/js/main.dde40f72.chunk.js"
+  },
+  {
+    "revision": "d209b6be4556c26de233",
+    "url": "./static/js/1.d209b6be.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "7e5114c80f69412ad523ffa837d23197",
+    "url": "./static/media/theinhardtreg-webfont.7e5114c8.ttf"
+  },
+  {
+    "revision": "749c0ebed81e657b50fef01aef82aa42",
+    "url": "./index.html"
+  }
+];
